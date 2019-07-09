c22dcdc8eac781181957650bf5adb0ec30fc1d41
Alberto <20015291@studenti.uniupo.it>
2019-07-09 19:55:41 +0200

    Metodo POST funzionante

--- src/main/java/it/uniupo/reti2/restAPI/GatewayAPI.java
+++ src/main/java/it/uniupo/reti2/restAPI/GatewayAPI.java
@@ -149,6 +149,28 @@ public class GatewayAPI {
             return finalJson;
         }, gson::toJson);
 
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         post(baseURL+"/buy", (req, res) -> {
             Map addRequest = gson.fromJson(req.body(), Map.class);
 
